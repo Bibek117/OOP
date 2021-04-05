c2ae7e1a48e9cfe7d71ea8f264aad2c5047ca753
Bibek_16 <71329649+Bibek117@users.noreply.github.com>
2021-04-05 20:50:27 +0545

    update

---
+++ Bibek/Bibek cpp lab report/lab2/lab)1.cpp
@@ -0,0 +1,16 @@
+//WAP to illustrate the use of this pointer
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+class result
+{
+  private:
+      char subject[20];
+      int marks;
+  public:
+    void setmarks(char sub[],int mark)
+    {
+
+    }
+};
